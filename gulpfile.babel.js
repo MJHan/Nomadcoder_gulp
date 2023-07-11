7bffa1ce1cd74f1b68d8f5720573d944d0d9d2e2
MJHan <hamatoto@mgail.com>
2023-07-11 13:59:58 +0900

    gulp-image 적용

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 
 const routes = {
   pug: {
     watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
   },
 };
 
@@ -17,12 +22,17 @@ const pug = () =>
 const clean = () => del([routes.pug.dest]);
 const webserver = () =>
   gulp.src([routes.pug.dest]).pipe(ws({ livereload: true, open: true }));
-const watch = () => gulp.watch(routes.pug.watch, pug);
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+};
 
 // task들을 그룹화해서 정의
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 const assets = gulp.series([pug]);
-const postDev = gulp.series([webserver, watch]); //gulp.parallel은 병렬실행(동시실행)시 사용, gulp.series는 순차실행 시 사용
+const postDev = gulp.parallel([webserver, watch]); //gulp.parallel은 병렬실행(동시실행)시 사용, gulp.series는 순차실행 시 사용
 
 // npm run dev : 그룹화한 task들을 순차적으로 실행
 export const dev = gulp.series([prepare, assets, postDev]);
